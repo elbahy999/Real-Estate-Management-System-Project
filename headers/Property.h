f794aad8c0bd4745c8c4c7cab6c20e17a4c39ada
Zyad Mahgoub <setimoulaic119@gmail.com>
2025-12-14 22:10:35 +0200

    Refactor Property class constructor and methods
    Refactored Property class methods for consistency and clarity.

--- headers/Property.h
+++ headers/Property.h
@@ -2,15 +2,23 @@
 #include <string>
 #include <iostream>
 using namespace std;
+
 class Property {
 private:
     double price;
     int id;
     string neighborhoodName;
-public:
-    Property(int id, double pr, const string& name) :price(pr), id(id), neighborhoodName(name) {}
-    int get_Property_ID()const { return id; }
-    double get_Price()const { return price; }
-    string  get_neighborhood_Name() { return neighborhoodName; }
-};
 
+public:
+    Property(int id, double pr, const string& name)
+        : price(pr), id(id), neighborhoodName(name) {
+    }
+
+    int get_Property_ID() const { return id; }
+    double get_Price() const { return price; }
+    string get_neighborhood_Name() const { return neighborhoodName; }
+
+    int getPropertyID() const { return id; }
+    double getPrice() const { return price; }
+    string getNeighborhoodName() const { return neighborhoodName; }
+};
