3593d6fd038b858c83ea5dfd6920af29b72c109e
Zyad Mahgoub <setimoulaic119@gmail.com>
2025-12-14 22:09:30 +0200

    Refactor DCLL class for better memory management
    Refactor DCLL class by implementing destructor, cleanup logic, and adding isEmpty method. Removed unused methods and improved memory management.

--- headers/DCLL.h
+++ headers/DCLL.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <stdexcept>
 #include <iostream>
 #include <string>
@@ -8,6 +7,7 @@
 #include <chrono>
 #include <fstream>
 using namespace std;
+
 template<typename Ty>
 struct PropertyNode {
     Ty data;
@@ -16,6 +16,7 @@ struct PropertyNode {
 
     PropertyNode(const Ty& prop) : data(prop), next(nullptr), prev(nullptr) {}
 };
+
 template <typename T>
 class DCLL {
 private:
@@ -24,7 +25,22 @@ private:
 
 public:
     DCLL() : tail(nullptr), count(0) {}
-    ~DCLL() {}
+
+    ~DCLL() {
+        if (tail == nullptr) return;
+
+        PropertyNode<T>* current = tail->next;
+        PropertyNode<T>* temp;
+
+        do {
+            temp = current;
+            current = current->next;
+            delete temp;
+        } while (current != tail->next);
+
+        tail = nullptr;
+        count = 0;
+    }
 
     void insert(const T& newProperty) {
         PropertyNode<T>* newNode = new PropertyNode<T>(newProperty);
@@ -33,14 +49,13 @@ public:
             tail = newNode;
             tail->next = tail;
             tail->prev = tail;
-
         }
         else {
             newNode->next = tail->next;
             newNode->prev = tail;
             tail->next->prev = newNode;
             tail->next = newNode;
-            tail = newNode;    // insert at end
+            tail = newNode;
         }
 
         count++;
@@ -54,7 +69,6 @@ public:
 
         do {
             if (current->data.getPropertyID() == propertyID) {
-
                 if (count == 1) {
                     delete current;
                     tail = nullptr;
@@ -70,7 +84,7 @@ public:
                 }
 
                 count--;
-                return;     // VERY IMPORTANT
+                return;
             }
 
             current = current->next;
@@ -114,129 +128,13 @@ public:
         return count;
     }
 
-    PropertyNode<T>* get_tail() {
+    PropertyNode<T>* get_tail() const {
         return tail;
     }
-};
 
-#pragma once
-
-#include <stdexcept>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <cmath>
-#include <chrono>
-#include <fstream>
-using namespace std;
-template<typename Ty>
-struct PropertyNode {
-    Ty data;
-    PropertyNode<Ty>* next;
-    PropertyNode<Ty>* prev;
-
-    PropertyNode(const Ty& prop) : data(prop), next(nullptr), prev(nullptr) {}
-};
-template <typename T>
-class DCLL {
-private:
-    PropertyNode<T>* tail;
-    int count;
-
-public:
-    DCLL() : tail(nullptr), count(0) {}
-    ~DCLL() {}
-
-    void insert(const T& newProperty) {
-        PropertyNode<T>* newNode = new PropertyNode<T>(newProperty);
-
-        if (tail == nullptr) {
-            tail = newNode;
-            tail->next = tail;
-            tail->prev = tail;
-
-        }
-        else {
-            newNode->next = tail->next;
-            newNode->prev = tail;
-            tail->next->prev = newNode;
-            tail->next = newNode;
-            tail = newNode;    // insert at end
-        }
-
-        count++;
-    }
-
-    void remove(int propertyID) {
-        if (tail == nullptr)
-            throw std::runtime_error("List is empty");
-
-        PropertyNode<T>* current = tail->next;
-
-        do {
-            if (current->data.getPropertyID() == propertyID) {
-
-                if (count == 1) {
-                    delete current;
-                    tail = nullptr;
-                }
-                else {
-                    current->prev->next = current->next;
-                    current->next->prev = current->prev;
-
-                    if (current == tail)
-                        tail = current->prev;
-
-                    delete current;
-                }
-
-                count--;
-                return;     // VERY IMPORTANT
-            }
-
-            current = current->next;
-
-        } while (current != tail->next);
-
-        throw std::runtime_error("Property ID not found");
-    }
-
-    T* search(int propertyID) const {
-        if (tail == nullptr)
-            throw std::runtime_error("List is empty");
-
-        PropertyNode<T>* current = tail->next;
-
-        do {
-            if (current->data.getPropertyID() == propertyID)
-                return &(current->data);
-
-            current = current->next;
-
-        } while (current != tail->next);
-
-        throw std::runtime_error("Property ID not found");
-    }
-
-    void traverse(void (*func)(const T&)) const {
-        if (tail == nullptr)
-            throw std::runtime_error("List is empty");
-
-        PropertyNode<T>* current = tail->next;
-
-        do {
-            func(current->data);
-            current = current->next;
-
-        } while (current != tail->next);
-    }
-
-    int get_Count() const {
-        return count;
-    }
-
-    PropertyNode<T>* get_tail() {
-        return tail;
+    bool isEmpty() const {
+        return tail == nullptr;
     }
 };
+
 
