00727e9bae78d7f62d01c1dfe42431c6f6c38c75
ZIad El-Bahy <118315573+elbahy999@users.noreply.github.com>
2025-12-14 19:36:10 +0200

    Add Property class with price and neighborhood attributes

---
+++ headers/Property.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include <iostream>
+    using namespace std;
+    class Property {
+    private:
+        double price;
+        int id;
+        string neighborhoodName;
+    public:
+        Property(int id, double pr, const string& name) :price(pr), id(id), neighborhoodName(name) {}
+        int get_Property_ID()const { return id; }
+        double get_Price()const { return price; }
+        string  get_neighborhood_Name() { return neighborhoodName; }
+    };
+
