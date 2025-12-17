9bdd7677710e3ed213e15132aed740205fce8f4f
Zikosallam1 <119742107+Zikosalam1@users.noreply.github.com>
2025-12-17 19:47:36 +0200

    Add files via upload

---
+++ Contributions/ziad mohamed .cpp
@@ -0,0 +1,54 @@
+#pragma once
+#include <iostream>
+#include "DCLL.h"
+#include "Property.h"
+#include "Neighborhood.h"
+#include "NeighborhoodIndex.h"
+using  namespace std;
+bool PropertyExists(DCLL<Property>& propertyList, int propertyID) {
+    try
+    {
+        propertyList.search(propertyID);
+        return true;
+    }
+    catch (...) {
+        return false;
+    }
+}
+bool IsvalidPropertyData(int propertyID, double price, const string& neighborhoodName)
+
+{ 
+if (propertyID <= 0)
+return false;
+
+if (price <= 0)
+return false;
+if (neighborhoodName == "")
+return false;
+
+return true;
+}
+void AddProperty(DCLL<Property>& propertyList, NeighborhoodIndex& neighborhoodIndex, int propertyID, double price,const string& neighborhoodName)
+{
+    if (!IsvalidPropertyData(propertyID, price, neighborhoodName)) {
+        return;
+    }
+    if (PropertyExists(propertyList, propertyID)) {
+        return;
+    }
+    Property newProperty(propertyID, price, neighborhoodName);
+    propertyList.insert(newProperty);
+    neighborhoodIndex.addPropertyToNeighborhood(newProperty);
+
+ }
+void RemoveProperty(DCLL<Property>& propertyList,NeighborhoodIndex& neighborhoodIndex,int propertyID) 
+{
+    if (!PropertyExists(propertyList, propertyID)) 
+    {
+        return;
+    }
+    Property*propertyToRemove = propertyList.search(propertyID);
+    string neighborhoodName = propertyToRemove->getNeighborhoodName();
+     propertyList.remove(propertyID);
+     neighborhoodIndex.removePropertyFromNeighborhood(propertyID, neighborhoodName);
+}
